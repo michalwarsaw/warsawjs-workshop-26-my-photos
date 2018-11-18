8c8fdccc3f463bef7611e8cc52c17c40d4a2e1ad
Michal Wlodarczyk <polskito@gmail.com>
2018-11-18 17:05:35 +0100

    first commit

---
+++ client/main.js
@@ -0,0 +1,58 @@
+function setup() {
+    fetchPhotos()
+        .then(photos => {
+            render(photos);
+            zoomPhoto(photos[0]);
+        });
+
+}
+
+function removeFullPhoto() {
+    const $full = document.querySelector('.full');
+    if ($full) {
+        $full.remove();
+    }
+}
+
+function zoomPhoto(photo) {
+    removeFullPhoto();
+    const $area = document.querySelector('.container');
+    const $img = document.createElement('img');
+    $img.classList.add('full');
+    $img.setAttribute('src', photo.image);
+    $area.appendChild($img);
+    displayPhotoDetails(photo);
+
+
+}
+
+function fetchPhotos() {
+
+    return fetch('http://localhost:3000/photos')
+        .then(res => res.json());
+
+}
+
+function render(photos) {
+    const $area = document.querySelector('.container');
+    photos.forEach((photo) => {
+        const $img = document.createElement('img');
+        $img.setAttribute('src', photo.thumb);
+        $area.appendChild($img);
+        $img.addEventListener('click', () => {
+            zoomPhoto(photo);
+        });
+    });
+
+}
+
+function displayPhotoDetails(photo) {
+    const template = `<p>${photo.tags.join(', ')}</p> <p>${photo.title}</p>`;
+    const $area = document.querySelector('.container');
+    const $div = document.createElement('.div');
+    $area.appendChild($div);
+
+    $area.innerHTML += template;
+}
+
+window.addEventListener('DOMContentLoaded', setup);
